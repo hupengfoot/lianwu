4bb740b5644ef7c202e8643c40d0c9d127f632d2
胡鹏 <hupenghp2005@gmail.com>
2017-03-19 17:06:08 +0800

    add log

--- app.js
+++ app.js
@@ -7,16 +7,23 @@ var util = require('util');
 var url = require('url');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+global.logConf = require(path.join(global.rootPath, "util/logConf"));
 var app = express();
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var morgan = require('morgan');
 
+var accessLog = morgan({
+        "format":"dev",
+        "stream":{write:function(str){console.info(str);}}
+});
+
+app.use(accessLog);
 app.use(bodyParser({limit : '1mb'}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser("ZC_COOKIE_STR_AQC"));
-
-app.get('/checksignature', function(req, res){
+app.get('/', function(req, res){
     var param = url.parse(req.url, true).query;
     console.log(param);
     res.jsonp(param.echostr);
